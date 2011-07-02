e96fb171fd2d132e2cd8523e6cf46ac46b53313d
Andrey A.I. Sitnik <andrey@sitnik.ru>
2011-07-02 13:37:41 +0400

    Add comments to user controller

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,10 +1,15 @@
+# Редакторы сайта
 class UsersController < ApplicationController
+  # Запрещаем все действия (кроме просмотра списка редакторов) посторонним
+  # посетителям
   before_filter :authenticate_user!, except: :index
   
+  # Список всех редакторов
   def index
     @users = User.order_by(name: :asc)
   end
   
+  # Изменение почты и имени редактора
   def update
     user = User.find(params[:id])
     user.name  = params[:user]['name']
@@ -12,10 +17,13 @@ class UsersController < ApplicationController
     if user.save
       redirect_to users_path
     else
+      # Если новые данные не проходят валидацию (например, почта пустая), то
+      # вывести сообщение об ошибке
       render :text => user.errors.full_messages, status: :bad_request
     end
   end
   
+  # Удаление редактора
   def destroy
     user = User.find(params[:id])
     user.destroy
