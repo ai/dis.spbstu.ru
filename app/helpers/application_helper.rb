de0fad1c86d95f9ee574d9975bac26d4941e627c
Andrey A.I. Sitnik <andrey@sitnik.ru>
2011-07-11 05:35:20 +0400

    Move layout helpers to layout_helper.rb

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,51 +1,16 @@
 # encoding: utf-8
+# Глобальные хелперы
 module ApplicationHelper
-  
-  # Возвращает путь, по которому можно исправить содержимое страницы
-  def edit_content_path(content)
-    return '/edit' if '/' == content.path
-    content.path + '/edit'
-  end
-  
-  # Добавляет ссылку на jQuery. В production использует сервера Яндекс, во время
-  # разработки использует локальную версию.
-  def include_jquery
-    if Rails.env.production?
-      url = 'http://yandex.st/jquery/1.6.2/jquery.min.js'
-    else
-      url = 'lib/development/jquery-1.6.2.js'
-    end
-    javascript_include_tag(url)
-  end
-  
-  # Добавляет дополнительные шрифты
-  def include_fonts
-    stylesheet_link_tag('http://fonts.googleapis.com/css?family=' +
-      'PT+Sans:400,400italic,700&subset=cyrillic,latin&v2')
-  end
-  
-  # JS-хак, который включает поддержку HTML5-тегов для IE 6—8
-  def enable_html5_for_ie
-    '<!--[if lt IE 9]>'.html_safe +
-      javascript_include_tag('//html5shim.googlecode.com/svn/trunk/html5.js') +
-    '<![endif]-->'.html_safe
-  end
-  
-  # В HTML5 больше не нужно писать type="text/javascript" для <script>.
-  # Добавляем хак, который вырезает лишний type из встроенного хелпера.
-  def javascript_include_tag(*params)
-    super.sub(' type="text/javascript"', '').html_safe
-  end
-  
-  # В HTML5 больше не нужно писать type="text/css" для <link rel="stylesheet">.
-  # Добавляем хак, который вырезает лишний type из встроенного хелпера.
-  def stylesheet_link_tag(*params)
-    super.sub(' type="text/css"', '').html_safe
-  end
   
   # Указывает заголовок веб-страницы. К нему будет ещё добавлено название сайта.
   def title(title)
     content_for(:title) { title }
+  end
+  
+  # Возвращает путь, по которому можно исправить содержимое страницы
+  def edit_content_path(path)
+    path = path.path if path.is_a? Content
+    (path.ends_with? '/') ? "#{path}edit" : "#{path}/edit"
   end
   
   # Вставляет в HAML класс, если условие в check верное
--- spec/helpers/application_helper_spec.rb
+++ spec/helpers/application_helper_spec.rb
@@ -5,12 +5,18 @@ describe ApplicationHelper do
   
   describe "edit_content_path" do
   
-    it "should return path to edit page" do
+    it "should return path to edit page by model" do
       content = Fabricate(:content, path: '/long/path')
       helper.edit_content_path(content).should == '/long/path/edit'
       
       root = Fabricate(:content, path: '/')
       helper.edit_content_path(root).should == '/edit'
+    end
+    
+    it "should return path to edit page by string" do
+      helper.edit_content_path('/long/path').should  == '/long/path/edit'
+      helper.edit_content_path('/long/path/').should == '/long/path/edit'
+      helper.edit_content_path('/').should == '/edit'
     end
   
   end
